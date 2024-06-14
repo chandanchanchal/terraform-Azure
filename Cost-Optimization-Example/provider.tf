cce658edd5ce9b072c7a9c0a5e112b3b95e6d02b
My Name <myemail@example.com>
2024-06-14 11:00:29 +0530

    d

---
+++ Cost-Optimization-Example/provider.tf
@@ -0,0 +1,27 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">= 3.106.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = ">= 3.6.2"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {
+    key_vault {
+      purge_soft_delete_on_destroy = true
+    }
+  }
+}
+
+
+
+
+
+
+
